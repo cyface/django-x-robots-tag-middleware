572cfc489e1aabbe1009bc50339bf67a1f5461b9
Tim White <tim@cyface.com>
2015-10-14 13:16:34 -0600

    Updating DB name & GitIgnore.

--- x_robots_tag_middleware_demo/settings.py
+++ x_robots_tag_middleware_demo/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'x_robots_tag_middleware_demo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'x_robots_tag_middleware_demo.db'),
     }
 }
 
