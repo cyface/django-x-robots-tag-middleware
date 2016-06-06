1424f7caf6b588c722d8702bb7e24a35b0fcb655
Tim White <tim@cyface.com>
2016-06-05 22:40:25 -0600

    Addd apps config.

---
+++ x_robots_tag_middleware/apps.py
@@ -0,0 +1,8 @@
+"""Django Apps Config"""
+
+from django.apps import AppConfig
+
+
+class XRobotsTagMiddlewareConfig(AppConfig):
+    name = 'x_robots_tag_middleware'
+    verbose_name = "X Robots Tag Middleware"
