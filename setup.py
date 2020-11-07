bbdeb08906d164a0e4785e68caffd4e0ba705ec5
Tim White <tim@cyface.com>
2020-11-07 09:43:07 -0700

    Readme update to make pypi happy.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="django-x-robots-tag-middleware",
     version="1.3.2",
     url="https://github.com/cyface/django-x-robots-tag-middleware",
-    license="BSD",
+    license="MIT",
     description="Enables returning the X-Robots-Tag header based on Django settings.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Tim White",
     author_email="tim@cyface.com",
     packages=find_packages(
@@ -21,10 +27,11 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Internet :: WWW/HTTP",
     ],
 )
